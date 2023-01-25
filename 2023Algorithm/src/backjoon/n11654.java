fd1cb4388cca390919e894d3cc3e0f8818ed7960
이지민 <68285285+jmlee119@users.noreply.github.com>
2023-01-25 23:09:10 +0900

    11654 아스키코드

---
+++ 2023Algorithm/src/backjoon/n11654.java
@@ -0,0 +1,18 @@
+package backjoon;
+
+import java.util.Scanner;
+
+public class n11654 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		char c = sc.nextLine().charAt(0);
+		int num = (int)c;
+		
+		System.out.println(num);
+		
+	}
+
+}

f77cd8e5975c11ef4e7601d5a6487ca7fbf4de1b
이지민 <68285285+jmlee119@users.noreply.github.com>
2023-02-04 23:51:16 +0900

    1193번 분수찾기

---
+++ 2023Algorithm/src/backjoon/n1193.java
@@ -0,0 +1,32 @@
+package backjoon;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class n1193 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+		int count = 0;
+		int i = 1;
+		int deno = 0;
+		int numer = 0;
+		while(count < x) {
+			count = count + i;
+			i++;
+		}
+        
+		if((i-1)%2 == 0) {
+			deno = 1 + (count - x);
+			numer = (i-1) - (count-x);
+		}else {
+			deno = (i-1) - (count-x);
+			numer = 1 + (count - x);
+		}
+		
+		
+		System.out.println(numer  + "/" + deno);
+    }
+}

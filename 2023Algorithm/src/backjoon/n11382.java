75a4a137cf86e02e8f197038298b6d4cbfc4287e
이지민 <68285285+jmlee119@users.noreply.github.com>
2023-03-02 13:57:47 +0900

    11382번 꼬마 정민

---
+++ 2023Algorithm/src/backjoon/n11382.java
@@ -0,0 +1,17 @@
+package backjoon;
+
+import java.util.Scanner;
+
+public class n11382 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		long a = sc.nextInt();
+		long b = sc.nextInt();
+		long c = sc.nextInt();
+		
+		System.out.println(a + b + c);
+	}
+
+}

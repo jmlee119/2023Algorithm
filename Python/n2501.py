fa892cf19ddbd7827c846e4c35ae8918c69d69e6
이지민 <68285285+jmlee119@users.noreply.github.com>
2023-03-30 23:58:22 +0900

    2501번 약수 구하기

--- Python/n2501.py
+++ Python/n2501.py
@@ -2,14 +2,12 @@ import sys
 input = sys.stdin.readline
 list = []
 n,k = map(int,input().rstrip().split())
-for i in range(1,n):
+for i in range(1,n+1):
     if(n%i==0):
         list.append(i)
-        if (i != n//i):
-            list.append(n//i)
+        
     
-list.sort()
-if (k >=len(list)):
+if (k >len(list)):
     print(0)
 else:
     print(list[k-1])

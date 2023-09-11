b58680eef35f56f16de98d3c307533e0f0ca0cc5
jmlee119 <jmlee119@naver.com>
2023-09-11 22:56:57 +0900

    동전 0

---
+++ Python/n11047.py
@@ -0,0 +1,15 @@
+N, K = map(int, input().split())
+coin = []
+for i in range(N):
+    coin.append(int(input()))
+
+coin.reverse()
+count = 0
+for i in coin:
+    if i <= K:
+        count += K // i
+        K = K % i
+        if K == 0:
+            break
+
+print(count)

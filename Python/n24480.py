8df10645f745b48ea3e4157acc2ba71bb12aa2d6
jmlee119 <jmlee119@naver.com>
2023-09-12 16:31:12 +0900

    알고리즘 수업 - 깊이 우선 탐색 2

---
+++ Python/n24480.py
@@ -0,0 +1,29 @@
+import sys
+
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+
+def dfs(a):
+    global cnt
+    visited[a] = cnt
+    line[a].sort(reverse=True)
+    for i in line[a]:
+        if visited[i] == 0:
+            cnt += 1
+            dfs(i)
+
+
+n, m, r = map(int, input().split())
+line = [[] for _ in range(n + 1)]
+visited = [0 for _ in range(n + 1)]
+cnt = 1
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    line[a].append(b)
+    line[b].append(a)
+dfs(r)
+# print(visited)
+for i in range(1, n + 1):
+    print(visited[i])

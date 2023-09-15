9633810f9ddf6064bc0f1d9abc5a4866c7595a63
jmlee119 <jmlee119@naver.com>
2023-09-15 14:01:23 +0900

    네트워크 연결

---
+++ Python/n1922.py
@@ -0,0 +1,42 @@
+import sys
+
+input = sys.stdin.readline
+
+V = int(input())
+E = int(input())
+edges = []
+answer = 0
+parent = [i for i in range(V + 1)]
+for _ in range(E):
+    a, b, c = map(int, input().split())
+    edges.append((a, b, c))
+
+edges.sort(key=lambda x: x[2])
+
+
+def find(a):
+    if parent[a] == a:
+        return a
+    parent[a] = find(parent[a])
+    return parent[a]
+
+
+def union(a, b):
+    a = find(a)
+    b = find(b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+for (
+    a,
+    b,
+    c,
+) in edges:
+    if find(a) != find(b):
+        union(a, b)
+        answer += c
+
+print(answer)

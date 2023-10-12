fff43c708197db2f0e0268416cf1cf39c3343902
jmlee119 <jmlee119@naver.com>
2023-10-12 15:58:38 +0900

    괄호

---
+++ Python/n9012.py
@@ -0,0 +1,24 @@
+import sys
+
+input = sys.stdin.readline
+T = int(input())
+for _ in range(T):
+    stack = input()
+    answer = []
+    for i in stack:
+        # print("answer: ", answer)
+        if i == "(":
+            answer.append(i)
+        elif i == ")":
+            if answer:
+                answer.pop()
+            else:
+                print("NO")
+                break
+    # print("answer: ", answer)
+    else:
+            
+        if not answer:
+            print("YES")
+        else:
+            print("NO")

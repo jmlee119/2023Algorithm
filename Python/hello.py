507e2edf4372e19ed0f2305a622e1ff1be0b7de3
jmlee119 <jmlee119@naver.com>
2023-10-26 21:26:40 +0900

    알고리즘 수업 - 너비 우선 탐색 1

--- Python/hello.py
+++
@@ -1,8 +0,0 @@
-def avg(*args):
-    sum=0
-    for i in args:
-        sum= sum + i
-    return sum/len(args)
-
-a=avg(4,5,7,8,9,5,4,8)
-print(a)
---
+++ Python/n24444.py
@@ -0,0 +1,35 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+n, m, k = map(int, input().split())
+visited = [0] * (n + 1)
+count = 1
+graph = [[] for _ in range(n + 1)]
+
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+
+def bfs(v):
+    global count
+    q = deque([k])
+    visited[k] = 1
+    while q:
+        v = q.popleft()
+        graph[v].sort()
+        for g in graph[v]:
+            if visited[g] == 0:
+                count += 1
+                visited[g] = count
+                q.append(g)
+
+
+bfs(k)
+
+for v in visited[1:]:
+    print(v)

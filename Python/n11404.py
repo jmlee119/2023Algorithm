09e6d3d884f16686ecb6bd0c3efdc8a4ef8e5d38
jmlee119 <jmlee119@naver.com>
2023-11-07 23:52:39 +0900

    플로이드

--- Python/n11404.py
+++ Python/n11404.py
@@ -1,36 +1,31 @@
-INF = int(1e9)  # 무한대 값 설정
+import sys
 
-n = int(input())  # 도시의 개수
-m = int(input())  # 버스의 개수
+input = sys.stdin.readline
 
-# 도시 간의 최소 비용을 저장할 2차원 배열 초기화
+INF = int(1e9)
+n = int(input())
+m = int(input())
+
 graph = [[INF] * (n + 1) for _ in range(n + 1)]
 
-# 자기 자신으로 가는 비용은 0으로 초기화
 for i in range(1, n + 1):
     for j in range(1, n + 1):
         if i == j:
             graph[i][j] = 0
 
-# 버스 정보 입력 및 초기화
 for _ in range(m):
     a, b, c = map(int, input().split())
-    # 같은 노선에 여러 버스가 있을 수 있으므로 최소 비용만 남기도록 함
-    if c < graph[a][b]:
-        graph[a][b] = c
+    graph[a][b] = min(c, graph[a][b])
 
-# 플로이드-와샬 알고리즘 수행
 for k in range(1, n + 1):
-    for i in range(1, n + 1):
-        for j in range(1, n + 1):
-            # i에서 j로 가는 경로보다 i에서 k를 거쳐 j로 가는 경로가 더 짧으면 업데이트
-            graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+    for a in range(1, n + 1):
+        for b in range(1, n + 1):
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
 
-# 결과 출력
 for i in range(1, n + 1):
     for j in range(1, n + 1):
         if graph[i][j] == INF:
-            print(0, end=" ")  # 도달할 수 없는 경우 0 출력
+            print(0, end=" ")
         else:
-            print(graph[i][j], end=" ")  # 최소 비용 출력
-    print()  # 줄 바꿈
+            print(graph[i][j], end=" ")
+    print()

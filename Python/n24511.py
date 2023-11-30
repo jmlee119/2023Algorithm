5618e827cf98e54dc8ee003e8441325d897ca273
jmlee119 <jmlee119@naver.com>
2023-11-30 15:28:07 +0900

    queuestack

---
+++ Python/n24511.py
@@ -0,0 +1,22 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+N = int(input())
+queuestack = list(map(int, input().split()))
+
+B = list(map(int, input().split()))
+M = int(input())
+C = list(map(int, input().split()))
+
+queue = deque([])
+
+for i in range(N):
+    if queuestack[i] == 0:
+        queue.appendleft(B[i])
+
+
+for i in range(M):
+    queue.append(C[i])
+    print(queue.popleft(), end=" ")

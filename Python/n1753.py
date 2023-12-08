79c85049f4960f623670855200b540941f695958
jmlee119 <jmlee119@naver.com>
2023-12-08 20:45:14 +0900

    최단경로

--- Python/n1753.py
+++ Python/n1753.py
@@ -2,17 +2,21 @@ import sys
 import heapq
 
 input = sys.stdin.readline
+
 INF = int(1e9)
 
 V, E = map(int, input().split())
-K = int(input())
+start = int(input())
 
-graph = [[] * (V + 1) for _ in range(V + 1)]
+graph = [[] for i in range(V + 1)]
+
 distance = [INF] * (V + 1)
 
-for i in range(E):
+for _ in range(E):
     a, b, c = map(int, input().split())
-    graph[a].append((b, c))
+    graph[a].append((c, b))
+
+    # heapq 쓰려면 앞이 가중치고 뒤가 ㅑndex여야 함.
 
 
 def dijkstra(start):
@@ -21,20 +25,20 @@ def dijkstra(start):
     distance[start] = 0
     while q:
         dist, now = heapq.heappop(q)
+
         if distance[now] < dist:
             continue
 
         for i in graph[now]:
-            cost = dist + i[1]
-            if cost < distance[i[0]]:
-                distance[i[0]] = cost
-                heapq.heappush(q, (cost, i[0]))
+            cost = i[0] + dist
+            if cost < distance[i[1]]:  # distance [now ] 가 아니였음
+                distance[i[1]] = cost
+                heapq.heappush(q, (cost, i[1]))
 
 
-dijkstra(K)
-
-
-for i in range(1, V + 1):
+dijkstra(start)
+# print(distance)
+for i in range(1, len(distance)):
     if distance[i] == INF:
         print("INF")
     else:

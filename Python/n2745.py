62f9cdbcb3486f7bb1233661682fa702917206bb
jmlee119 <jmlee119@naver.com>
2023-12-13 15:22:14 +0900

    배열 합치기

---
+++ Python/n2745.py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+N, B = map(str, input().split())
+B = int(B)
+
+N = N[::-1]
+JIN = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+result = 0
+
+for i in range(len(N) - 1, -1, -1):
+    result += (B**i) * JIN.index(N[i])
+
+print(result)
+
+# 60466175

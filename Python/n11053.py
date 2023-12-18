1bdef694e6755e0fa1f960cc0e86520bf7f12044
jmlee119 <jmlee119@naver.com>
2023-12-18 16:04:52 +0900

    가장 긴 증가하는 부분 수열

---
+++ Python/n11053.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+
+A = int(input())
+arr = list(map(int, input().split()))
+
+dp = [1] * 1001
+
+for i in range(A):
+    for j in range(i):
+        if arr[i] > arr[j]:
+            dp[i] = max(dp[i], dp[j] + 1)
+
+print(max(dp))

e8a5f0813f28406f07caa661197b0e5adc077994
jmlee119 <jmlee119@naver.com>
2024-10-16 13:44:07 +0900

    삼각형 외우기

--- Python/n10101.py
+++ Python/n10101.py
@@ -5,11 +5,12 @@ a = int(input())
 b = int(input())
 c = int(input())
 
-if a == 60 and b == 60 and c == 60:
-    print("Equilateral")
-elif (a + b + c) == 180 and a == b or b == c or a == c:
-    print("Isosceles")
-elif (a + b + c) == 180:
-    print("Scalene")
+if a + b + c == 180:
+    if a == b == c == 60:
+        print("Equilateral")
+    elif a == b or b == c or a == c:
+        print("Isosceles")
+    else:
+        print("Scalene")
 else:
     print("Error")

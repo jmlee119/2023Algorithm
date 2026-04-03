2d79755b426550405ad022ef40da8f5f4ca99db4
jmlee119 <jmlee119@naver.com>
2026-04-03 13:19:33 +0900

    약수

---
+++ 2023Algorithm/src/backjoon/n1037.java
@@ -0,0 +1,21 @@
+package backjoon;
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class n1037 {
+    public static void main(String args[]) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bf.readLine());
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        for (int i=0; i<n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        System.out.println(arr[0] * arr[arr.length - 1]);
+    }
+}
